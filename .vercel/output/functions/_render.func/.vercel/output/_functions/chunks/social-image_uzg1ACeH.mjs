cecdb603b19fdbd69316912e2a97bf3d197b3a1d
caio <oliveiracaio1@yandex.com>
2025-02-08 00:14:49 +0100

    removed .vercel

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/social-image_uzg1ACeH.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./social-image_COcEk_xm.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const collectedScripts = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
